9e9d03953627a1c1d1cc730c2132bcbd9cf9b7da
Carla Gómez <carla.gomez.abad@gmail.com>
2023-03-07 19:24:36 +0100

    front

--- src/app/interfaces/i-event.ts
+++
@@ -1,9 +0,0 @@
-export interface IEvent { //i-product.ts
-  id?: number;
-  title: string;
-  descr: string;
-  studio: string;
-  releaseDate: Date;
-  imageUrl: string;
-  rating: number;
-  }
--- src/app/pipes/studio-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StudioFilterPipe } from './studio-filter.pipe';
-
-describe('StudioFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StudioFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
